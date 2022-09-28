b3a479113bd95e7acc150359d48e6940c9045f55
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-09-28 00:06:58 -0700

    navbar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <h1>hey yo</h1>
+      <Navbar />
     </div>
   );
 }
