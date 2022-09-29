ff3e23451a744f1661af5f9b1a9ccfb6d667c74d
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-09-29 02:46:23 -0700

    footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Destinations from './components/destinations/Destinations'
 import Search from './components/search/Search'
 import Selects from './components/selects/Selects'
 import ImgCarousel from './components/carousel/ImgCarousel'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Search />
       <Selects />
       <ImgCarousel />
+      <Footer />
     </div>
   );
 }
